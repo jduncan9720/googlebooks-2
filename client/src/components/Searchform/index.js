c1b510d81e8ced947ecd16071eb8e8f602087127
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-07-23 21:58:13 -0600

    adding search route

--- client/src/components/Searchform/index.js
+++ client/src/components/Searchform/index.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import axios from "axios"
 
 export default class Searchbar extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            input: ""
+        }
+        this.handleChange=this.handleChange.bind(this)
+    };
+
+    handleChange(event) {
+        this.setState({
+            input:event.target.value
+        })
+    }
+
+    bookSearch(event){
+        event.preventdefault()
+            this.setState({
+                input: ""
+            })
+            //searchBooks route and add this.state.input as parameter
+    }
+
     render() {
         return (
             <div id="searchBar">
-                <input type="text" id="bookInput" placeholder="Book Title" size="100"></input> 
+                <input type="text" id="bookInput" placeholder="Book Title" size="100" onChange={this.handleChange} value={this.state.input}/>
                 <button type="submit">Search</button>
             </div>
         )
